681638193d9e5d225e41b060477862446f36b00a
jinchao <370696614@qq.com>
2019-12-06 00:36:53 +0800

    页面列表查询对接后台接口

--- src/module/cms/api/cms.js
+++ src/module/cms/api/cms.js
@@ -1,9 +1,11 @@
 //public是对axios的工具类封装，定义了http请求方法
 import http from './../../../base/api/public'
-
+import querystring from 'querystring'
 let sysConfig = require('@/../config/sysConfig')
 let apiUrl = sysConfig.xhApiUrlPre;
-
-export const page_list = (page,size) =>{
-    return http.requestQuickGet(apiUrl+'/cms/page/list/'+page+"/"+size)
+//页面查询
+export const page_list = (page,size,params) =>{
+    //将params对象数据拼装成key/value串
+    let queryString =  querystring.stringify(params);
+    return http.requestQuickGet(apiUrl+'/cms/page/list/'+page+"/"+size+"?"+queryString)
 }
